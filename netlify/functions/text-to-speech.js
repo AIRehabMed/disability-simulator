1a12835a47746436b9880c93711970ad2d1a6460
AIRehabMed <bradlandry@gmail.com>
2026-02-13 13:12:11 -0500

    Update text-to-speech.js

--- netlify/functions/text-to-speech.js
+++ netlify/functions/text-to-speech.js
@@ -20,7 +20,7 @@ exports.handler = async (event, context) => {
       },
       body: JSON.stringify({
         text: text,
-        model_id: 'eleven_monolingual_v1',
+        model_id: 'eleven_turbo_v2_5',
         voice_settings: {
           stability: 0.5,
           similarity_boost: 0.75
